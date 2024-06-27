c7d243f042850024851a5c929e061bd082cf029b
Александр Черников <a.chernikov65@gmail.com>
2024-06-27 22:12:17 +0300

    add function mask_account_card

--- src/masks.py
+++ src/masks.py
@@ -26,3 +26,16 @@ def get_mask_account(account_number: str) -> str:
         else:
             mask_account_number.append(num)
     return "".join(mask_account_number)
+
+
+def mask_account_card(account_card: str) -> str:
+    parts = account_card.split()
+    name = " ".join(parts[:-1])
+    number = parts[-1]
+
+    if name.startswith("Счет"):
+        masked_number = get_mask_account(number)
+    else:
+        masked_number = get_mask_card_number(number)
+
+    return f"{name} {masked_number}"
