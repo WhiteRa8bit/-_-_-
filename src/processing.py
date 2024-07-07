554a5423324358a75dd70d9ce498b97db2e62653
Александр Черников <a.chernikov65@gmail.com>
2024-07-07 14:53:24 +0300

    add func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -6,3 +6,9 @@ def filter_by_state(list_for_filter: list, state: str) -> list:
         if elem.get("state") == state:
             new_list.append(elem)
     return new_list
+
+
+def sort_by_date(list_for_sort: list, reverse_sort: bool = True) -> list:
+    """Сортирует список словарей по ключу date. Возвращает новый список по убыванию, опционально для reverse_sort"""
+    sorted_list = sorted(list_for_sort, key=lambda x: x["date"], reverse=reverse_sort)
+    return sorted_list
