f8bfa5981a7af6b380654209192115d1291a8f7f
Александр Черников <a.chernikov65@gmail.com>
2024-07-07 14:36:00 +0300

    black src/processing.pyfixes

--- src/processing.py
+++ src/processing.py
@@ -3,7 +3,7 @@ def filter_by_state(list_for_filter: list, state: str) -> list:
     содержащий только те словари, у которых ключ state"""
     new_list = []
     for elem in list_for_filter:
-        if elem.get('state') == state:
+        if elem.get("state") == state:
             new_list.append(elem)
     return new_list
 
