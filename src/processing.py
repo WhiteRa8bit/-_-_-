a294774959261561b6e6fdbd97e1589c909115a4
Александр Черников <a.chernikov65@gmail.com>
2024-07-07 18:37:31 +0300

    funcs fixes

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(list_for_filter: list, state: str) -> list:
+def filter_by_state(list_for_filter: list, state: str = 'EXECUTED') -> list:
     """Принемает список словарей и возвращает новый список словарей,
     содержащий только те словари, у которых ключ state"""
     new_list = []
@@ -8,7 +8,7 @@ def filter_by_state(list_for_filter: list, state: str) -> list:
     return new_list
 
 
-def sort_by_date(list_for_sort: list, reverse_sort: bool = True) -> list:
+def sort_by_date(list_for_sort: list, ascending: bool = True) -> list:
     """Сортирует список словарей по ключу date. Возвращает новый список по убыванию, опционально для reverse_sort"""
-    sorted_list = sorted(list_for_sort, key=lambda x: x["date"], reverse=reverse_sort)
+    sorted_list = sorted(list_for_sort, key=lambda x: x["date"], reverse=ascending)
     return sorted_list
